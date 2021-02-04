3062bc65826e2219da69e81baa55233d78d3b9fe
Divy-22 <divypagariya11@gmail.com>
2021-02-04 14:41:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/notes/Note.kt
@@ -0,0 +1,10 @@
+package com.example.android.notes
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+class Note(@ColumnInfo(name = "text")val text: String) {
+    @PrimaryKey(autoGenerate = true) var id = 0;
+}
